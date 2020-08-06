c5a645abdd5d05845933fd19feb221c5457aaf9e
Alex Noponen <alexnoponen@gmail.com>
2020-08-05 21:39:34 -0600

    Add files via upload

---
+++ guessgame.py
@@ -0,0 +1,25 @@
+def player_guess():
+	guess = ''
+	while guess not in ['0','1','2']:
+		guess = input("Pick a number: 0, 1, or 2")
+	return int(guess)
+
+def check_guess(mylist,guess):
+    if mylist[guess] == 'O':
+        print("Correct!")
+    else:
+        print("Wrong guess!")
+        print(mylist)
+
+#inital list
+mylist = [' ','O',' ']
+
+#shuffle list
+mixedup_list = shuffle_list(mylist)
+
+#user guess
+guess = player_guess()
+
+#check guess
+check_guess(mixedup_list,guess)
+
